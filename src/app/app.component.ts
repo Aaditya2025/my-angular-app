63e7b7398e0e9d198f3e6964950cbc2a8c4b8840
Aditya Yadav <adityayadav135952@gmail.com>
2025-06-04 19:29:10 +0530

    learning events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,45 +1,49 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  imports: [],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
-})
-export class AppComponent {
-  // handleClickEvent(){
-  //   console.log("Function Called!");
-  //   this.otherFunction(); 
-  // }
+// @Component({
+//   selector: 'app-root',
+//   imports: [],
+//   templateUrl: './app.component.html',
+//   styleUrl: './app.component.css'
+// })
+// export class AppComponent {
+//   // handleClickEvent(){
+//   //   console.log("Function Called!");
+//   //   this.otherFunction(); 
+//   // }
 
-  // otherFunction(){
-  //   console.log("other function called!");
-  // }
-  count = 0;
+//   // otherFunction(){
+//   //   console.log("other function called!");
+//   // }
+//   count = 0;
 
-  // handleIncrement() {
-  //   this.count = this.count + 1;
-  // }
+//   // handleIncrement() {
+//   //   this.count = this.count + 1;
+//   // }
 
-  // handleReset() {
-  //   this.count = 0;
-  // }
+//   // handleReset() {
+//   //   this.count = 0;
+//   // }
 
-  // handleDecrement() {
-  //   this.count = this.count - 1;
-  // }
+//   // handleDecrement() {
+//   //   this.count = this.count - 1;
+//   // }
 
-  handleCount(val: string) {
-    if (val == "minus") {
-      if(this.count >0){
-        this.count = this.count - 1;
-      }else{
-        this.count = 0;
-      }
-    } else if (val == "plus") {
-      this.count = this.count + 1;
-    } else {
-      this.count = 0;
-    }
-  }
-}
+//   handleCount(val: string) {
+//     if (val == "minus") {
+//       if(this.count >0){
+//         this.count = this.count - 1;
+//       }else{
+//         this.count = 0;
+//       }
+//     } else if (val == "plus") {
+//       this.count = this.count + 1;
+//     } else {
+//       this.count = 0;
+//     }
+//   }
+// }
+
+
+
+// Events in Angular
